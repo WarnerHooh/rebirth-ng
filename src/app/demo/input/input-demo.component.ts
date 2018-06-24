8f3de25b70583f4f4c2ce1262d6ece5dc174db2a
WarnerHooh <Warner.Hooh@gmail.com>
2018-06-24 22:03:35 +0800

    [Input] demo update

--- src/app/demo/input/input-demo.component.ts
+++ src/app/demo/input/input-demo.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 're-input-demo',
-  templateUrl: './input-demo.component.html'
+  templateUrl: './input-demo.component.html',
+  styleUrls: ['./input-demo.component.scss'],
 })
 export class InputDemoComponent {
 
