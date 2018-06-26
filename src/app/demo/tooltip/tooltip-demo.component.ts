5d97aea4d520b4e6cec2b0fe5c2362cfec410c9e
WarnerHooh <Warner.Hooh@gmail.com>
2018-06-26 21:49:50 +0800

    [Tooltip] doc update

--- src/app/demo/tooltip/tooltip-demo.component.ts
+++ src/app/demo/tooltip/tooltip-demo.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { TooltipDirective } from 'rebirth-ng';
 
 @Component({
   selector: 're-tooltip-demo',
@@ -7,5 +8,16 @@ import { Component } from '@angular/core';
 export class TooltipDemoComponent {
   setting = { name: 'rebirth tooltip', placement: 'top', trigger: 'hover' };
   github = 'https://github.com/greengerong/rebirth-ng';
+
+  @ViewChild('tooltip') tooltip: TooltipDirective;
+  tooltipSwitch: boolean;
+
+  toggleToolTip() {
+    if (this.tooltipSwitch) {
+      this.tooltip.show();
+    } else {
+      this.tooltip.hide();
+    }
+  }
 
 }
--- src/app/demo/tooltip/tooltip.module.ts
+++ src/app/demo/tooltip/tooltip.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { TooltipDemoComponent } from './tooltip-demo.component';
 import { CommonModule } from '@angular/common';
 import { RebirthNGModule } from 'rebirth-ng';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, RebirthNGModule],
+  imports: [CommonModule, RebirthNGModule, FormsModule],
   exports: [TooltipDemoComponent],
   declarations: [TooltipDemoComponent],
   providers: [],
