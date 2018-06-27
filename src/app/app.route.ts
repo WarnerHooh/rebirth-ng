6ebcc05196dd10cca8f99ac32104c09e78389c6d
WarnerHooh <Warner.Hooh@gmail.com>
2018-06-28 01:58:12 +0800

    [Portal] link update

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { PortalComponent } from './page/portal/portal.component';
 
 export const ROUTER_CONFIG: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: '/portal'},
-  {path: 'portal', component: PortalComponent},
+  {path: '', pathMatch: 'full', component: PortalComponent},
   {path: 'design', loadChildren: 'app/page/main/main.module#MainModule'},
 ];
