d70752c00a4034fa41ee215bf4209b6c92a1641d
WarnerHooh <Warner.Hooh@gmail.com>
2018-06-28 14:05:54 +0800

    [Badge] styling update

--- src/app/demo/badge/badge-demo.component.ts
+++ src/app/demo/badge/badge-demo.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 're-badge-demo',
-  templateUrl: './badge-demo.component.html'
+  templateUrl: './badge-demo.component.html',
+  styleUrls: ['./badge-demo.scss'],
 })
 export class BadgeDemoComponent {
 
